12fa083c1e3dfa3fdec5611297e3df5be4df0fbb
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2025-04-09 17:05:38 +0530

    fixed apn_add

--- src/apn/x64/apn_add_sub/apn_add_sub.c
+++ src/apn/x64/apn_add_sub/apn_add_sub.c
@@ -4,31 +4,35 @@
 
 extern u8 _adc_add_n(u64* result, const u64* op1, const u64* op2, u64 size);
 extern u8 _sbb_sub_n(u64* result, const u64* op1, const u64* op2, u64 size);
-extern u8 _adc_add_one(u64* result, const u64* op1, u64 size, u64 val);
-extern u8 _sbb_sub_one(u64* result, const u64* op1, u64 size, u64 val);
 
 /* PUBLIC API FUNCTIONS */
 u8 apn_add_n(u64* result, const u64* op1, const u64* op2, u64 size)
 {
+	APAC_ASSERT(size != 0);
+
 	return _adc_add_n(result, op1, op2, size);
 }
 
 u8 apn_add(u64* result, const u64* op1, const u64* op2, u64 size1, u64 size2)
 {
-	u64 max_size = (size1 < size2 ? size2 : size1);
-	u64 min_size = (size1 < size2 ? size1 : size2);
-	u64* max_ptr = (size1 < size2 ? op2 : op1);
-	
-	u8 carry = _adc_add_n(result, op1, op2, min_size);
+	APAC_ASSERT(size1 != 0 && size2 != 0);
+	APAC_ASSERT(size1 >= size2);
 
-	return _adc_add_one(&result[min_size], &max_ptr[min_size], max_size - min_size, (u64)carry);
+	u8 carry = _adc_add_n(result, op1, op2, size2);
+	u64 counter = size2;
+
+	while (carry && counter < size1)
+	{
+		carry = _addcarry_u64(carry, op1[counter], 0, &result[counter]);
+		counter++;
+	}
+
+	if (counter != size1)
+	{
+		apn_cpy(&result[counter], &op1[counter], size1 - counter);
+	}
+	return carry;
 }
-
-u8 apn_add_one(u64* result, const u64* op1, u64 size, u64 val)
-{
-	return _adc_add_one(result, op1, size, val);
-}
-
 
 
 
