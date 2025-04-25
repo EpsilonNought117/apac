4be6565ced63c3f0cf5c482b03b6b8130223582a
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2025-04-25 16:37:24 +0530

    updated karatsuba threshold and added comments to karatsuba func

--- src/apn/x64/apn_mul/_karatsuba_mul.c
+++ src/apn/x64/apn_mul/_karatsuba_mul.c
@@ -10,18 +10,27 @@ void _apn_additive_karatsuba_mul(
 	u64* workspace
 )
 {
-	u8	allocd_here = 0; // variable to indicate whether to free workspace
+	u8 allocd_here = 0;
+	
+	// variable to indicate whether to free workspace
 	// if it was allocated within the function here or if some 
 	// higher function gave it needed amount
 
 	if (!depth && !workspace)
 	{
-		u64 temp_val = size / KARATSUBA_MUL_THRESHOLD;
+		u64 temp_val = size / KARATSUBA_MUL_THRESHOLD; 
+
+		// do log2(temp_val) + 1 to determine how many times
+		// the functions recurses on the worst skew of the
+		// recursion tree
+
 		uint32_t log_part = 0;
 
 		_BitScanReverse64(&log_part, temp_val);
 		log_part++;
-		u64 workspace_size = ((u64)log_part * 12 + (size >> 1) * 8);
+		u64 workspace_size = ((u64)log_part * 12 + ((size + 1) >> 1) * 8);
+
+		// explained more in the documentations
 
 		workspace = apac_malloc(sizeof(u64) * workspace_size);
 
@@ -31,19 +40,27 @@ void _apn_additive_karatsuba_mul(
 			abort();
 		}
 
-		allocd_here = 1; // set flag to indicate memory allocated here
-		apn_set(workspace, workspace_size, 0);
+		allocd_here = 1; // set flag to indicate memory allocated
+		apn_set(workspace, workspace_size, 0); // clear out workspace
 	}
 
 	if (size < KARATSUBA_MUL_THRESHOLD)
 	{
+		// for sizes below threshold
+		// use the basecase multiplication
+
 		_apn_basecase_mul(result, op1, op2, size, size);
 		return;
 	}
 	else
 	{
-		u64 lower = (size + 1) >> 1;
-		u64 upper = size >> 1;
+		u64 lower = (size + 1) >> 1;	// upper half of the operands
+		u64 upper = size >> 1;			// lower half of the operands
+
+		// the lower half is at most 1 limb larger than upper half
+		// (size + 1) / 2 = floor(size / 2)
+		// size / 2 = ceil(size / 2)
+		// (a / b) is integer division so it truncates the fraction part
 
 		_apn_additive_karatsuba_mul(
 			&result[0],
@@ -52,7 +69,10 @@ void _apn_additive_karatsuba_mul(
 			lower,
 			depth + 1,
 			&workspace[0]
-		);
+		);	// result[0 : (2 * lower)] = a0 * b0
+		// a0 is op1[0 : lower]
+		// b0 is op2[0 : lower]
+		// this is c0
 
 		_apn_additive_karatsuba_mul(
 			&result[2 * lower],
@@ -61,13 +81,20 @@ void _apn_additive_karatsuba_mul(
 			upper,
 			depth + 1,
 			&workspace[0]
-		);
+		); // result[(2 * lower) : (2 * size)] = a1 * b1
+		// a1 is op1[(lower + 1) : upper]
+		// b1 is op2[(lower + 1) : upper]
+		// this is c1
 
-		u8 cy1 = apn_add(&workspace[0], &op1[0], &op1[lower], lower, upper);
-		workspace[lower] += cy1;
+		u8 carry1 = apn_add(&workspace[0], &op1[0], &op1[lower], lower, upper);
+		workspace[lower] += carry1;
 
-		u8 cy2 = apn_add(&workspace[lower + 1], &op2[0], &op2[lower], lower, upper);
-		workspace[2 * lower + 1] += cy2;
+		// propagate carry if occured
+
+		u8 carry2 = apn_add(&workspace[lower + 1], &op2[0], &op2[lower], lower, upper);
+		workspace[2 * lower + 1] += carry2;
+
+		// same as above here
 
 		_apn_additive_karatsuba_mul(
 			&workspace[2 * (lower + 1)],
@@ -76,7 +103,11 @@ void _apn_additive_karatsuba_mul(
 			lower + 1,
 			depth + 1,
 			&workspace[4 * (lower + 1)]
-		);
+		); // workspace[2 * (lower + 1) : 4 * (lower + 1)] = (a1 + a0) * (b1 + b0)
+		// (a1 + a0) is workspace[0 : (lower + 1)]
+		// (b1 + b0) is workspace[(lower + 1) : 2 * (lower + 1)]
+
+		// c2 = (a1 + a0) * (b1 + b0)
 
 		apn_sub(
 			&workspace[2 * (lower + 1)],
@@ -84,7 +115,7 @@ void _apn_additive_karatsuba_mul(
 			&result[0],
 			2 * (lower + 1),
 			2 * lower
-		);
+		); // c2 -= c0
 
 		apn_sub(
 			&workspace[2 * (lower + 1)],
@@ -92,7 +123,9 @@ void _apn_additive_karatsuba_mul(
 			&result[2 * lower],
 			2 * (lower + 1),
 			2 * upper
-		);
+		); // c2 -= c1
+
+		// at this point c2 = c2 - c0 - c1
 
 		apn_add_n(
 			&result[lower],
@@ -100,12 +133,25 @@ void _apn_additive_karatsuba_mul(
 			&workspace[2 * (lower + 1)],
 			2 * lower + 1
 		);
+		// add c2 to result[lower : 2 * lower + 1]
+		
+		//   [----- c0 -----]
+		// +          [----- c2 -----]
+		// + 		    	    [----- c1 -----]
+		// -------------------------------------
+		// [-------------- result --------------]
 
 		apn_set(&workspace[0], 4 * (lower + 1), 0);
+		
+		// clear workspace for upcoming recursive calls
 	}
 
 	if (!depth && allocd_here)
 	{
+		// deallocate aux storage if it was
+		// allocated within the function itself and
+		// not provided by some caller
+
 		apac_free(workspace);
 		workspace = NULL;
 	}
