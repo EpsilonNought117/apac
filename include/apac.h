fe53125310769147c9d597ab1181d7c742e7c3db
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2025-06-11 11:37:30 +0530

    support for linux dropped due to issues with NASM

--- include/apac.h
+++ include/apac.h
@@ -11,45 +11,22 @@
 #include <limits.h>
 
 /****************************************************************************************************/
-/*******************    COMPILER AND OS SPECIFIC HEADERS AND IMPORT/EXPORTS      ********************/
+/***********************    COMPILER SPECIFIC HEADERS AND IMPORT/EXPORTS     ************************/
 /****************************************************************************************************/
 
-#if defined(_WIN64)
-    
-    #if defined(_MSC_VER) && (defined(_M_X64) || defined(_M_AMD64))
+#if defined(_M_X64) || defined(_M_AMD64)
+	#include <intrin.h>
+#endif
 
-        #include <intrin.h>
-    
-    #endif
-
-    #if defined(BUILD_SHARED_LIBS)
-        // Export symbols when building the DLL
-        #define APAC_API __declspec(dllexport)
-    #elif defined(LIBAPAC_DLL)
-        // Import symbols when using the DLL
-        #define APAC_API __declspec(dllimport)
-    #else
-        // Static library, no import/export needed
-        #define APAC_API
-    #endif
-
-#elif defined(__linux__)
-
-    #if (defined(__GNUC__) || defined(__clang__)) &&    \
-        (defined(__X86_64__) || defined (__amd64__) || defined (__x86_64) || defined(__amd64))
-
-        #include <x86intrin.h>
-
-    #endif
-
-    #if defined(BUILD_SHARED_LIBS)
-        // Export symbols when building the shared library
-        #define APAC_API __attribute__((visibility("default")))
-    #else
-        // Static library, no export needed
-        #define APAC_API
-    #endif
-
+#if defined(BUILD_SHARED_LIBS)
+	// Export symbols when building the DLL
+	#define APAC_API __declspec(dllexport)
+#elif defined(LIBAPAC_DLL)
+	// Import symbols when using the DLL
+	#define APAC_API __declspec(dllimport)
+#else
+	// Static library, no import/export needed
+	#define APAC_API
 #endif
 
 /****************************************************************************************************/
@@ -58,32 +35,32 @@
 
 typedef enum apac_err
 {
-    APAC_OK,
-    APAC_OOM,
-    APAC_MATH_ERR
+	APAC_OK,
+	APAC_OOM,
+	APAC_MATH_ERR
 
 }   apac_err;
 
 #if !defined(APAC_DISABLE_ASSERT)
 
-    #ifndef APAC_REPORT_ERR
-        #define APAC_REPORT_ERR(x) \
+#ifndef APAC_REPORT_ERR
+#define APAC_REPORT_ERR(x) \
         fprintf(stderr, "APAC ERROR %s:%d %s\n", __FILE__, __LINE__, x);
-    #endif
+#endif
 
-    #ifndef APAC_ASSERT
-    #define APAC_ASSERT(x)      \
+#ifndef APAC_ASSERT
+#define APAC_ASSERT(x)      \
         if (!(x))               \
         {                       \
             APAC_REPORT_ERR(#x) \
             abort();            \
         }
-    #endif
+#endif
 
 #else
 
-    #define APAC_REPORT_ERR(x) ((void)0)
-    #define APAC_ASSERT(x)     ((void)0)
+#define APAC_REPORT_ERR(x) ((void)0)
+#define APAC_ASSERT(x)     ((void)0)
 
 #endif
 
@@ -96,9 +73,9 @@ APAC_API extern void* (*apac_realloc)(void*, size_t);
 APAC_API extern void (*apac_free)(void*);
 
 APAC_API void apacSetMemFuncs(
-    void* (*ptr1)(size_t),
-    void* (*ptr2)(void*, size_t),
-    void (*ptr3)(void*)
+	void* (*ptr1)(size_t),
+	void* (*ptr2)(void*, size_t),
+	void (*ptr3)(void*)
 );
 
 APAC_API void apacInit(void);
@@ -118,15 +95,15 @@ typedef int8_t     i8;
 
 typedef struct
 {
-    u64 karatsuba_mul_n_threshold;
-    u64 toom33_mul_n_threshold;
+	u64 karatsuba_mul_n_threshold;
+	u64 toom33_mul_n_threshold;
 
-    u8(*__apn_add_n_ptr)(u64*, const u64*, const u64*, u64);
-    u8(*__apn_sub_n_ptr)(u64*, const u64*, const u64*, u64);
-    void (*__apn_mul_bc_ptr)(u64*, const u64*, const u64*, u64, u64);
-    void (*__apn_neg_ptr)(u64*, const u64*, u64);
-    void (*__apn_cpy_ptr)(u64*, const u64*, u64);
-    void (*__apn_set_ptr)(u64*, u64, u64);
+	u8(*__apn_add_n_ptr)(u64*, const u64*, const u64*, u64);
+	u8(*__apn_sub_n_ptr)(u64*, const u64*, const u64*, u64);
+	void (*__apn_mul_bc_ptr)(u64*, const u64*, const u64*, u64, u64);
+	void (*__apn_neg_ptr)(u64*, const u64*, u64);
+	void (*__apn_cpy_ptr)(u64*, const u64*, u64);
+	void (*__apn_set_ptr)(u64*, u64, u64);
 
 }   __apac_cpu_params;
 
@@ -136,84 +113,84 @@ extern __apac_cpu_params curr_cpu;
 /*********************************          APN FUNCTIONS         ***********************************/
 /****************************************************************************************************/
 
-/**               
+/**
 *                   IMPORTANT NOTES
-* 
+*
 * 1) THESE FUNCTIONS DO NOT PERFORM ANY MEMORY ALLOCATIONS.
 *
 * 2) ASSERTS ARE USED IN DEBUG MODE FOR CATCHING INVALID ARGUMENT ERRORS.
-* 
+*
 * 3) THE RESULT MUST HAVE APPROPRIATE NUMBER OF LIMBS ACCORDING TO OPERATION.
-* 
+*
 * 4) THEY DO NOT PERFORM ANY BOUNDS CHECKING.
-* 
+*
 * 5) LITTLE TO NO WORK IS DONE APART FROM THE ACTUAL COMPUTATION NEEDED.
-* 
+*
 * 6) NO SIZE ARGUMENT SHOULD BE ZERO, ALWAYS PASS AT LEAST SIZE 1 FOR ALL SIZE ARGUMENTS.
-* 
+*
 */
 
 /*
-    1) result must have "size" limbs
-    2) returns the carry out
+	1) result must have "size" limbs
+	2) returns the carry out
 */
 APAC_API u8 apn_add_n(u64* result, const u64* op1, const u64* op2, u64 size);
 
 /*
-    1) size1 must be greater than or equal to size2.
-    2) result must have size1 limbs
-    3) returns the carry out
+	1) size1 must be greater than or equal to size2.
+	2) result must have size1 limbs
+	3) returns the carry out
 */
 APAC_API u8 apn_add(u64* result, const u64* op1, const u64* op2, u64 size1, u64 size2);
 
 /*
-    1) result must have "size" limbs
-    2) returns the carry out (unlikely in avg case)
+	1) result must have "size" limbs
+	2) returns the carry out (unlikely in avg case)
 */
 APAC_API u8 apn_add_one(u64* result, const u64* op1, u64 size, u64 val);
 
 /*
-    1) result must have "size" limbs
-    2) returns the borrow out
+	1) result must have "size" limbs
+	2) returns the borrow out
 */
 APAC_API u8 apn_sub_n(u64* result, const u64* op1, const u64* op2, u64 size);
 
 /*
-    1) size1 must be greater than or equal to size2.
-    2) result must have size1 limbs
-    3) return the borrow out
+	1) size1 must be greater than or equal to size2.
+	2) result must have size1 limbs
+	3) return the borrow out
 */
 APAC_API u8 apn_sub(u64* result, const u64* op1, const u64* op2, u64 size1, u64 size2);
 
 /*
-    1) result must have "size" limbs
-    2) returns the borrow out
+	1) result must have "size" limbs
+	2) returns the borrow out
 */
 APAC_API u8 apn_sub_one(u64* result, const u64* op1, u64 size, u64 val);
 
 /*
-    1) result must have "size" number of limbs
+	1) result must have "size" number of limbs
 */
 APAC_API void apn_cpy(u64* result, const u64* op1, u64 size);
 
 /*
-    1) result must have "size" number of limbs
+	1) result must have "size" number of limbs
 */
 APAC_API void apn_neg(u64* result, const u64* op1, u64 size);
 
 /*
-    1) No overlap permitted between result and either of the operands
-    2) result must have 2 * size number of limbs
+	1) No overlap permitted between result and either of the operands
+	2) result must have 2 * size number of limbs
 */
 APAC_API void apn_mul_n(u64* result, const u64* op1, const u64* op2, u64 size);
 
 /*
-    1) Result must have size number of limbs
+	1) Result must have size number of limbs
 */
 APAC_API void apn_set(u64* result, u64 size, u64 val);
 
 /*
-    1) returns 1, 0, -1 based on whether op1 > op2, op1 = op2 or op1 < op2 
+	1) returns 1, 0, -1 based on whether op1 > op2, op1 = op2 or op1 < op2
 */
 APAC_API i8 apn_cmp(const u64* op1, const u64* op2, u64 size1, u64 size2);
 
--- src/apn/x64/_add/_add_n.asm
+++ src/apn/x64/_add/_add_n.asm
@@ -5,15 +5,9 @@
 ;   |                                                                           |
 ;   O---------------------------------------------------------------------------O
 
-BITS 64
+.code
 
-section .text 
-
-global _add_n_8unroll
-global _add_n_4unroll
-global _add_n_2unroll
-
-%ifdef _WIN64
+    option casemap:none
 
     ;   Microsoft ABI 
 
@@ -26,14 +20,16 @@ global _add_n_2unroll
 
 ;   8x Unroll Variant
 
-_add_n_8unroll:
+_add_n_8unroll PROC FRAME
+.pushframe
+.endprolog
 
     mov     r11, r9
     shr     r9,  3      ; size /= 8 
     and     r11, 7      ; size %= 8
-    jz      .before_big_loop
+    jz      before_big_loop
 
-.small_loop:
+small_loop:
 
     mov     rax, QWORD [rdx]
     adc     rax, QWORD [r8]
@@ -43,16 +39,16 @@ _add_n_8unroll:
     lea     r8,  [r8  + 8]
     lea     rcx, [rcx + 8]
     dec     r11
-    jnz     .small_loop
+    jnz     small_loop
 
-.before_big_loop:
+before_big_loop:
 
     setc    al
     test    r9, r9
     bt      ax, 0
-    jz      .func_done
+    jz      func_done
 
-.big_loop:
+big_loop:
 
     mov     rax, QWORD [rdx]
     adc     rax, QWORD [r8]
@@ -90,23 +86,27 @@ _add_n_8unroll:
     lea     r8,  [r8  + 64]
     lea     rcx, [rcx + 64]
     dec     r9
-    jnz     .big_loop
+    jnz     big_loop
 
-.func_done:
+func_done:
 
     setc    al
     ret     
 
+_add_n_8unroll ENDP
+
 ;   4x Unroll Variant
 
-_add_n_4unroll:
+_add_n_4unroll PROC FRAME
+.pushframe
+.endprolog
 
     mov     r11, r9
     shr     r9,  2      ; size /= 4 
     and     r11, 3      ; size %= 4
-    jz      .before_big_loop
+    jz      before_big_loop
 
-.small_loop:
+small_loop:
 
     mov     rax, QWORD [rdx]
     adc     rax, QWORD [r8]
@@ -116,16 +116,16 @@ _add_n_4unroll:
     lea     r8,  [r8  + 8]
     lea     rcx, [rcx + 8]
     dec     r11
-    jnz     .small_loop
+    jnz     small_loop
 
-.before_big_loop:
+before_big_loop:
 
     setc    al
     test    r9, r9
     bt      ax, 0
-    jz      .func_done
+    jz      func_done
 
-.big_loop:
+big_loop:
 
     mov     rax, QWORD [rdx]
     adc     rax, QWORD [r8]
@@ -147,24 +147,28 @@ _add_n_4unroll:
     lea     r8,  [r8  + 32]
     lea     rcx, [rcx + 32]
     dec     r9
-    jnz     .big_loop
+    jnz     big_loop
 
-.func_done:
+func_done:
 
     setc    al
     ret 
 
+_add_n_4unroll ENDP
+
 ;   2x Unroll Variant
 
-_add_n_2unroll:
+_add_n_2unroll PROC FRAME
+.pushframe
+.endprolog
 
     xor     rax, rax    ; carry holder/propagator
     mov     r11, r9
     shr     r9,  1      ; size /= 2
     and     r11, 1      ; size %= 2
-    jz      .before_big_loop
+    jz      before_big_loop
 
-.one_iter:
+one_iter:
 
     mov     rax, QWORD [rdx]
     adc     rax, QWORD [r8]
@@ -175,14 +179,14 @@ _add_n_2unroll:
     lea     rcx, [rcx + 8]
     dec     r11
 
-.before_big_loop:
+before_big_loop:
 
     setc    al
     test    r9, r9
     bt      ax, 0
-    jz      .func_done
+    jz      func_done
 
-.big_loop:
+big_loop:
 
     mov     rax, QWORD [rdx]
     adc     rax, QWORD [r8]
@@ -196,196 +200,11 @@ _add_n_2unroll:
     lea     r8,  [r8  + 16]
     lea     rcx, [rcx + 16]
     dec     r9
-    jnz     .big_loop
+    jnz     big_loop
 
-.func_done:
+func_done:
 
     setc    al
     ret 
 
-%elifdef __linux__
-
-    ; SystemV ABI
-
-    ;   Function Arguments
-    ;
-    ;   rdi -> result (u64*)   
-    ;   rsi -> op1 (const u64*)
-    ;   rcx -> op2 (const u64*)
-    ;   rdx -> size (u64)
-    
-;   8x Unroll Variant
-
-_add_n_8unroll:
-
-    mov     r8,  rdx
-    shr     rdx, 3
-    and     r8,  7
-    jz      .before_big_loop
-
-.small_loop:
-
-    mov     rax, QWORD [rsi]
-    adc     rax, QWORD [rcx]
-    mov     QWORD [rdi], rax
-
-    lea     rsi, [rsi + 8]
-    lea     rcx, [rcx + 8]
-    lea     rdi, [rdi + 8]
-    dec     r8
-    jnz     .small_loop    
-
-.before_big_loop:
-
-    setc    al
-    test    rdx, rdx
-    bt      ax, 0
-    jz      .func_done
-
-.big_loop:
-
-    mov     rax, QWORD [rsi]
-    adc     rax, QWORD [rcx]
-    mov     QWORD [rdi], rax
-
-    mov     rax, QWORD [rsi + 8]
-    adc     rax, QWORD [rcx + 8]
-    mov     QWORD [rdi + 8], rax
-
-    mov     rax, QWORD [rsi + 16]
-    adc     rax, QWORD [rcx + 16]
-    mov     QWORD [rdi + 16], rax
-
-    mov     rax, QWORD [rsi + 24]
-    adc     rax, QWORD [rcx + 24]
-    mov     QWORD [rdi + 24], rax
-
-    mov     rax, QWORD [rsi + 32]
-    adc     rax, QWORD [rcx + 32]
-    mov     QWORD [rdi + 32], rax
-
-    mov     rax, QWORD [rsi + 40]
-    adc     rax, QWORD [rcx + 40]
-    mov     QWORD [rdi + 40], rax
-
-    mov     rax, QWORD [rsi + 48]
-    adc     rax, QWORD [rcx + 48]
-    mov     QWORD [rdi + 48], rax
-
-    mov     rax, QWORD [rsi + 56]
-    adc     rax, QWORD [rcx + 56]
-    mov     QWORD [rdi + 56], rax
-
-    lea     rsi, [rsi + 64]
-    lea     rcx, [rcx + 64]
-    lea     rdi, [rdi + 64]
-    dec     rdx
-    jnz     .big_loop 
-
-.func_done:
-
-    setc    al
-    ret     
-
-_add_n_4unroll:
-
-    mov     r8,  rdx
-    shr     rdx, 2
-    and     r8,  3
-    jz      .before_big_loop
-
-.small_loop:
-
-    mov     rax, QWORD [rsi]
-    adc     rax, QWORD [rcx]
-    mov     QWORD [rdi], rax
-
-    lea     rsi, [rsi + 8]
-    lea     rcx, [rcx + 8]
-    lea     rdi, [rdi + 8]
-    dec     r8
-    jnz     .small_loop
-
-.before_big_loop:
-
-    setc    al
-    test    rdx, rdx
-    bt      ax, 0
-    jz      .func_done
-
-.big_loop:
-
-    mov     rax, QWORD [rsi]
-    adc     rax, QWORD [rcx]
-    mov     QWORD [rdi], rax
-
-    mov     rax, QWORD [rsi + 8]
-    adc     rax, QWORD [rcx + 8]
-    mov     QWORD [rdi + 8], rax
-
-    mov     rax, QWORD [rsi + 16]
-    adc     rax, QWORD [rcx + 16]
-    mov     QWORD [rdi + 16], rax
-
-    mov     rax, QWORD [rsi + 24]
-    adc     rax, QWORD [rcx + 24]
-    mov     QWORD [rdi + 24], rax
-
-    lea     rsi, [rsi + 32]
-    lea     rcx, [rcx + 32]
-    lea     rdi, [rdi + 32]
-    dec     rdx
-    jnz     .big_loop
-
-.func_done:
-
-    setc    al
-    ret         
-
-_add_n_2unroll:
-
-    mov     r8,  rdx
-    shr     rdx, 1
-    and     r8,  1
-    jz      .before_big_loop
-
-.one_iter:
-
-    mov     rax, QWORD [rsi]
-    adc     rax, QWORD [rcx]
-    mov     QWORD [rdi], rax
-
-    lea     rsi, [rsi + 8]
-    lea     rcx, [rcx + 8]
-    lea     rdi, [rdi + 8]
-    dec     r8
-
-.before_big_loop:
-
-    setc    al
-    test    rdx, rdx
-    bt      ax, 0
-    jz      .func_done    
-
-.big_loop:
-
-    mov     rax, QWORD [rsi]
-    adc     rax, QWORD [rcx]
-    mov     QWORD [rdi], rax
-
-    mov     rax, QWORD [rsi + 8]
-    adc     rax, QWORD [rcx + 8]
-    mov     QWORD [rdi + 8], rax
-
-    lea     rsi, [rsi + 16]
-    lea     rcx, [rcx + 16]
-    lea     rdi, [rdi + 16]
-    dec     rdx
-    jnz     .big_loop
-
-.func_done:
-
-    setc    al
-    ret
-
-%endif
+_add_n_2unroll ENDP
