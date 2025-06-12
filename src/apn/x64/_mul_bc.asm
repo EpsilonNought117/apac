be8f1cda18e89850c34ca2350164b0853ed0b834
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2025-06-12 08:39:48 +0530

    refactored _mul_bc funcs into one file

--- src/apn/x64/_mul_bc.asm
+++ src/apn/x64/_mul_bc.asm
@@ -20,6 +20,529 @@
     ;   ASSUMPTION
     ;   (size1 >= size2)
 
+;   -------------------------
+;
+;        MULX/ADOX/ADCX        
+;
+;   -------------------------
+
+; The fastest basecase mul procedues with 2x, 4x and 8x unrolled variants
+; Compatible on processors supporting ADX and BMI2 ISA Extensions
+
+;   8x Unroll Variant
+
+_mul_bc_mulx_adx_8unroll PROC FRAME
+
+    push    rbx
+.pushreg    rbx
+    push    rdi
+.pushreg    rdi
+    push    rsi
+.pushreg    rsi
+    push    r12
+.pushreg    r12
+    push    r13
+.pushreg    r13
+    push    r14
+.pushreg    r14
+    push    r15
+.pushreg    r15
+    push    rbp
+.pushreg    rbp
+.pushframe
+.endprolog
+
+    xor     rax, rax    ; i
+    xchg    rbp, rcx    ; result in rbp
+    xchg    rbx, rdx    ; op1 in rbx
+    mov     r10, QWORD PTR [rsp + 104] ; size2 in r10
+    mov     r12, r9
+    shr     r9,  3      ; size1 /= 8
+    and     r12, 7      ; size1 %= 8
+    mov     r13, rbp    ; store a copy of result in r13
+    mov     r11, rbx    ; store a copy of op1 in r11
+
+loop_outer:
+
+    mov     rcx, r12
+    mov     rdx, QWORD PTR [r8  + rax*8] ; load op2[i] into rdx
+    lea     rbp, [r13 + rax*8]           
+    lea     rbx, [r11]
+
+inner_small:
+
+    jrcxz   before_inner_unrolled
+
+    mulx    rdi, rsi, QWORD PTR [rbx]
+
+    adcx    rsi, QWORD PTR [rbp]
+    adox    rdi, QWORD PTR [rbp + 8]
+
+    mov     QWORD PTR [rbp], rsi
+    mov     QWORD PTR [rbp + 8], rdi    
+
+    lea     rbx, [rbx + 8]
+    lea     rbp, [rbp + 8]
+    lea     rcx, [rcx - 1]
+    jmp     inner_small
+
+before_inner_unrolled:
+
+    mov     rcx, r9
+    jmp     inner_unrolled
+
+outer_loop_end:
+
+    adcx    r15, QWORD PTR [rbp]
+    mov     QWORD PTR [rbp], r15
+
+    inc     rax
+    cmp     r10, rax
+    jz      end_of_func
+    jmp     loop_outer
+
+ALIGN 16
+inner_unrolled:
+
+    jrcxz   outer_loop_end
+
+    mulx    rdi, rsi, QWORD PTR [rbx]
+    adcx    rsi, QWORD PTR [rbp]
+    adox    rdi, QWORD PTR [rbp + 8]
+    mov     QWORD PTR [rbp], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 8]
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 16]
+    mov     QWORD PTR [rbp + 8], rsi
+    mov     r14, rdi
+    
+    mulx    rdi, rsi, QWORD PTR [rbx + 16]
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 24]
+    mov     QWORD PTR [rbp + 16], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 24]
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 32]
+    mov     QWORD PTR [rbp + 24], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 32]
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 40]
+    mov     QWORD PTR [rbp + 32], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 40]
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 48]
+    mov     QWORD PTR [rbp + 40], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 48]
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 56]
+    mov     QWORD PTR [rbp + 48], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 56]
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 64]
+    mov     QWORD PTR [rbp + 56], rsi
+    mov     QWORD PTR [rbp + 64], rdi
+
+    lea     rbx, [rbx + 64]
+    lea     rbp, [rbp + 64]
+    lea     rcx, [rcx - 1]
+    jmp     inner_unrolled
+
+end_of_func:
+
+    pop     rbp
+    pop     r15
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     rsi
+    pop     rdi
+    pop     rbx
+    ret     
+
+_mul_bc_mulx_adx_8unroll ENDP
+
+;   4x Unroll Variant
+
+_mul_bc_mulx_adx_4unroll PROC FRAME
+
+    push    rbx
+.pushreg    rbx
+    push    rdi
+.pushreg    rdi
+    push    rsi
+.pushreg    rsi
+    push    r12
+.pushreg    r12
+    push    r13
+.pushreg    r13
+    push    r14
+.pushreg    r14
+    push    r15
+.pushreg    r15
+    push    rbp
+.pushreg    rbp
+.pushframe
+.endprolog
+
+    xor     rax, rax    ; i
+    xchg    rbp, rcx    ; result in rbp
+    xchg    rbx, rdx    ; op1 in rbx
+    mov     r10, QWORD PTR [rsp + 104] ; size2 in r10
+    mov     r12, r9
+    shr     r9,  2      ; size1 /= 4
+    and     r12, 3      ; size1 %= 4
+    mov     r13, rbp    ; store a copy of result in r13
+    mov     r11, rbx    ; store a copy of op1 in r11
+    mov     r15, jump_table
+    lea     r15, [r15 + r12*8]
+
+loop_outer:
+
+    mov     rdx, QWORD PTR [r8  + rax*8] ; load op2[i] into rdx
+    lea     rbp, [r13 + rax*8]           
+    lea     rbx, [r11]
+    mov     rcx, r9    
+    xor     r12, r12
+    jmp     [r15]
+
+rem1:
+
+    mulx    rdi, rsi, QWORD PTR [rbx]
+
+    adcx    rsi, QWORD PTR [rbp]
+    adox    rdi, QWORD PTR [rbp + 8]
+
+    mov     QWORD PTR [rbp], rsi
+    mov     QWORD PTR [rbp + 8], rdi
+
+    lea     r11, [r11 + 8]
+    lea     r13, [r13 + 8]
+    jmp     loop_unrolled_4
+
+rem2:
+
+    mulx    rdi, rsi, QWORD PTR [rbx]
+
+    adcx    rsi, QWORD PTR [rbp]
+    adox    rdi, QWORD PTR [rbp + 8]
+
+    mov     QWORD PTR [rbp], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 8]
+
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 16]
+
+    mov     QWORD PTR [rbp + 8], rsi
+    mov     QWORD PTR [rbp + 16], rdi
+
+    lea     r11, [r11 + 16]
+    lea     r13, [r13 + 16]
+    jmp     loop_unrolled_4
+
+rem3:
+
+    mulx    rdi, rsi, QWORD PTR [rbx]
+    
+    adcx    rsi, QWORD PTR [rbp]
+    adox    rdi, QWORD PTR [rbp + 8]
+
+    mov     QWORD PTR [rbp], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 8]
+
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 16]
+
+    mov     QWORD PTR [rbp + 8], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 16]
+
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 24]
+
+    mov     QWORD PTR [rbp + 16], rsi
+    mov     QWORD PTR [rbp + 24], rdi
+
+    lea     r11, [r11 + 24]
+    lea     r13, [r13 + 24]
+    jmp     loop_unrolled_4
+
+loop_end:
+
+    adcx    r12, QWORD PTR [rbp]
+    mov     QWORD PTR [rbp], r12
+
+    inc     rax
+    cmp     r10, rax
+    jz      end_of_func
+    jmp     loop_outer
+
+ALIGN 16
+loop_unrolled_4:
+
+    jrcxz   loop_end
+
+    mulx    rdi, rsi, QWORD PTR [rbx]
+
+    adcx    rsi, QWORD PTR [rbp]
+    adox    rdi, QWORD PTR [rbp + 8]
+
+    mov     QWORD PTR [rbp], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 8]
+
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 16]
+
+    mov     QWORD PTR [rbp + 8], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 16]
+
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 24]
+
+    mov     QWORD PTR [rbp + 16], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 24]
+
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 32]
+
+    mov     QWORD PTR [rbp + 24], rsi
+    mov     QWORD PTR [rbp + 32], rdi
+
+    lea     r11, [r11 + 32]
+    lea     r13, [r13 + 32]
+    lea     rcx, [rcx - 1]
+    jmp     loop_unrolled_4
+
+jump_table:
+            dq offset loop_unrolled_4
+            dq offset rem1
+            dq offset rem2
+            dq offset rem3
+    
+end_of_func:
+
+    pop     rbp
+    pop     r15
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     rsi
+    pop     rdi
+    pop     rbx
+    ret     
+
+_mul_bc_mulx_adx_4unroll ENDP
+
+; 2x Unroll Variant
+
+_mul_bc_mulx_adx_2unroll PROC FRAME
+
+    push    rbx
+.pushreg    rbx
+    push    rdi
+.pushreg    rdi
+    push    rsi
+.pushreg    rsi
+    push    r12
+.pushreg    r12
+    push    r13
+.pushreg    r13
+    push    r14
+.pushreg    r14
+    push    r15
+.pushreg    r15
+    push    rbp
+.pushreg    rbp
+.pushframe
+.endprolog
+
+    xor     rax, rax    ; i
+    xchg    rbp, rcx    ; result in rbp
+    xchg    rbx, rdx    ; op1 in rbx
+    mov     r10, QWORD PTR [rsp + 104]  ; size2 in r10
+    mov     r12, r9
+    shr     r9,  1      ; size1 /= 2
+    and     r12, 1      ; size1 %= 2
+    mov     r13, rbp    ; store a copy of result in r13
+    mov     r11, rbx    ; store a copy of op1 in r11
+
+loop_outer:
+
+    mov     rdx, QWORD PTR [r8  + rax*8] ; load op2[i] into rdx
+    lea     rbp, [r13 + rax*8]           
+    lea     rbx, [r11]
+    mov     rcx, r9    
+    xor     r15, r15
+    test    r12, r12
+    jz      loop_unrolled_twice
+
+one_iter:
+
+    mulx    rdi, rsi, QWORD PTR [rbx]
+
+    adcx    rsi, QWORD PTR [rbp]
+    adox    rdi, QWORD PTR [rbp + 8]
+
+    mov     QWORD PTR [rbp], rsi
+    mov     QWORD PTR [rbp + 8], rdi
+
+    lea     r11, [r11 + 8]
+    lea     r13, [r13 + 8]
+    jmp     loop_unrolled_twice
+
+loop_end:
+
+    adcx    r15, QWORD PTR [rbp]
+    mov     QWORD PTR [rbp], r15
+
+    inc     rax
+    cmp     r10, rax
+    jz      end_of_func
+    jmp     loop_outer
+
+loop_unrolled_twice:
+
+    jrcxz   loop_end
+
+    mulx    rdi, rsi, QWORD PTR [rbx]
+
+    adcx    rsi, QWORD PTR [rbp]
+    adox    rdi, QWORD PTR [rbp + 8]
+
+    mov     QWORD PTR [rbp], rsi
+    mov     r14, rdi
+
+    mulx    rdi, rsi, QWORD PTR [rbx + 8]
+
+    adcx    rsi, r14
+    adox    rdi, QWORD PTR [rbp + 16]
+
+    mov     QWORD PTR [rbp + 8], rsi
+    mov     QWORD PTR [rbp + 16], rdi
+
+    lea     r11, [r11 + 16]
+    lea     r13, [r13 + 16]
+    lea     rcx, [rcx - 1]
+    jmp     loop_unrolled_twice
+
+end_of_func:
+
+    pop     rbp
+    pop     r15
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     rsi
+    pop     rdi
+    pop     rbx
+    ret     
+
+_mul_bc_mulx_adx_2unroll ENDP
+
+;   -------------------------
+;
+;           MULX/ADC        
+;
+;   -------------------------
+
+;   This procedure is not particularly optimized as it is for 
+;   Haswell processors which I haven't had a chance to test.
+;   Haswell was the only generation which supported BMI2 but
+;   not ADX. Can be optimized as per need in future.
+
+_mul_bc_mulx PROC FRAME
+
+	push	rbx
+.pushreg	rbx
+	push	rdi
+.pushreg	rdi
+	push	rsi
+.pushreg	rsi
+	push	r12
+.pushreg	r12
+	push	r13
+.pushreg	r13
+.pushframe
+.endprolog
+
+	xor		rax, rax	; i
+	xchg	rbx, rdx	; rbx <- op1 (const u64*)
+
+loop_outer:
+
+	xor		r12, r12	; temp_reg		
+	xor		r11, r11	; j
+	xor		rsi, rsi	; low64
+	xor		rdi, rdi	; high64
+	mov		r10, rax	; indexer for result
+	mov		r13, r9		; size1 in r13 for jnz
+	mov		rdx, QWORD PTR [r8 + rax*8] ; op2[i]
+	
+loop_inner:
+	
+	adc		r12, rdi
+	mulx	rdi, rsi, QWORD PTR [rbx + r11*8]	; rdi:rsi = rdx * op1[j]
+
+	add		r12, rsi
+	adc		rdi, 0
+	add		QWORD PTR [rcx + r10*8], r12
+
+	mov		r12, 0
+	inc		r11
+	inc		r10
+	dec		r13
+	jnz		loop_inner
+
+loop_end:
+	
+	mov		r12, QWORD PTR [rsp + 80]	; size2 into r12 temporarily
+	adc		QWORD PTR [rcx + r10*8], rdi
+	inc		rax
+	cmp		r12, rax
+	jnz		loop_outer
+
+end_of_func:
+
+	pop		r13
+	pop		r12
+	pop		rsi
+	pop		rdi
+	pop		rbx
+	ret		0
+
+_mul_bc_mulx ENDP
+
+;   -------------------------
+;
+;            MUL/ADC        
+;
+;   -------------------------
+
+; Lowest common denominator multiplication routine
+; Not particularly optimized as this was pre-Haswell
+
 _mul_bc PROC FRAME
     
     push    rbx
