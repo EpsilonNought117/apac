0d746adeb572116b16b2b5e876a258c7a67f7368
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2025-12-08 10:38:40 +0530

    hidden_helpers now has macros for UMUL128, ADC64, SBB64

--- src/apn/commons/hidden_helpers.h
+++ src/apn/commons/hidden_helpers.h
@@ -9,6 +9,15 @@
 
 		#if defined(_M_X64) || defined(_M_AMD64)
 
+			#define SBB64(borrow, op1, op2, result)		\
+					({ borrow = _subborrow_u64((borrow), (op1), (op2), &(result)); })
+
+			#define ADC64(carry, op1, op2, result)		\
+					({ carry = _addcarry_u64((carry), (op1), (op2), &(result)); })
+
+			#define UMUL128(op1, op2, low64, high64)	\
+					({ low64 = _umul128((op1), (op2), &(high64)); })
+
 			#define CLZ64(value)	\
 					({ uint32_t _idx; _BitScanReverse64(&_idx, (value)) ? (int)_idx : -1; })
 
@@ -29,8 +38,21 @@
 
 	#if defined(__GNUC__) || defined(__clang__)
 
+		#define SBB64(borrow, op1, op2, result)								\
+				({ result = __builtin_subcll((op1), (op2), (borrow), &(borrow)); })
+
+		#define ADC64(carry, op1, op2, result)								\
+				({ result = __builtin_addcll((op1), (op2), (carry), &(carry)); })
+
 		#define CLZ64(value)	\
 				((value) ? (63 - __builtin_clzll(value)) : -1)
+
+		#define UMUL128(op1, op2, low64, high64)					\
+				({													\
+					__uint128_t val = (__uint128_t)(op1) * (op2);	\
+					low64 = (apn_seg_t)(val);						\
+					high64 = (apn_seg_t)(val >> 64);				\
+				})
 
         #if defined(__x86_64)   || defined(__amd64)   || \
             defined(__x86_64__) || defined(__amd64__)
