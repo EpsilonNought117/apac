9aa084c8817caa126a2cb8472e6817ef21e45ac7
The Blessed Machine <devanshmehrotra117@gmail.com>
2025-12-14 13:24:41 +0530

    added illegal overlap asserts

--- src/apn/commons/apn_add.c
+++ src/apn/commons/apn_add.c
@@ -3,9 +3,9 @@
 extern apac_cpu_params curr_cpu;
 
 apn_seg_t apn_add_n(
-	apn_seg_t* result, 
-	const apn_seg_t* op1, 
-	const apn_seg_t* op2, 
+	apn_seg_t* result,
+	const apn_seg_t* op1,
+	const apn_seg_t* op2,
 	apn_size_t size
 )
 {
@@ -13,8 +13,18 @@ apn_seg_t apn_add_n(
 	APAC_ASSERT(result != NULL);
 	APAC_ASSERT(op1 != NULL);
 	APAC_ASSERT(op2 != NULL);
-	APAC_ASSERT(curr_cpu.apn_add_n_ptr != NULL);
-	
+
+	APAC_ASSERT(!(result > op1 && result < op1 + size),
+		"(result) overlaps the middle of (op1). Allowed only if (result) <= (op1) "
+		"or (result) >= (op1 + size)");
+
+	APAC_ASSERT(!(result > op2 && result < op2 + size),
+		"(result) overlaps the middle of (op2). Allowed only if (result) <= (op2) "
+		"or (result) >= (op2 + size)");
+
+	APAC_ASSERT(curr_cpu.apn_add_n_ptr != NULL,
+		"apacGetCPUSpec() or apacInit() not called");
+
 	apn_seg_t carry = curr_cpu.apn_add_n_ptr(result, op1, op2, size);
 	return carry;
 }
@@ -32,7 +42,16 @@ apn_seg_t apn_add(
 	APAC_ASSERT(result != NULL);
 	APAC_ASSERT(op1 != NULL);
 	APAC_ASSERT(op2 != NULL);
-	APAC_ASSERT(curr_cpu.apn_add_n_ptr != NULL);
+	APAC_ASSERT(curr_cpu.apn_add_n_ptr != NULL, "apacGetCPUSpec() or apacInit() not called");
+
+	// Prevent partial overlaps with op1 and op2
+	APAC_ASSERT(!(result > op1 && result < op1 + size1),
+		"(result) overlaps the middle of (op1). Allowed only if (result) <= (op1) "
+		"or (result) >= (op1 + size1)");
+
+	APAC_ASSERT(!(result > op2 && result < op2 + size2),
+		"(result) overlaps the middle of (op2). Allowed only if (result) <= (op2) "
+		"or (result) >= (op2 + size2)");
 
 	apn_seg_t carry = curr_cpu.apn_add_n_ptr(result, op1, op2, size2);
 
@@ -44,16 +63,21 @@ apn_seg_t apn_add(
 }
 
 apn_seg_t apn_add_one(
-	apn_seg_t* result, 
-	const apn_seg_t* op1, 
-	apn_size_t size, 
+	apn_seg_t* result,
+	const apn_seg_t* op1,
+	apn_size_t size,
 	apn_seg_t val
 )
 {
 	APAC_ASSERT(size != 0);
 	APAC_ASSERT(op1 != NULL);
 	APAC_ASSERT(result != NULL);
-	APAC_ASSERT(curr_cpu.apn_add_one_ptr != NULL);
+	APAC_ASSERT(curr_cpu.apn_add_one_ptr != NULL, "apacGetCPUSpec() or apacInit() not called");
+
+	// Prevent partial overlap between result and op1
+	APAC_ASSERT(!(result > op1 && result < op1 + size),
+		"(result) overlaps the middle of (op1). Allowed only if (result) <= (op1) "
+		"or (result) >= (op1 + size)");
 
 	apn_seg_t carry = curr_cpu.apn_add_one_ptr(result, op1, size, val);
 	return carry;
--- src/apn/commons/apn_addmul_one.c
+++ src/apn/commons/apn_addmul_one.c
@@ -9,11 +9,17 @@ apn_seg_t apn_addmul_one(
 	apn_seg_t val
 )
 {
+	APAC_ASSERT(size != 0);
 	APAC_ASSERT(result != NULL);
 	APAC_ASSERT(op1 != NULL);
-	APAC_ASSERT(size != 0);
-	APAC_ASSERT((result <= op1) || (result >= (op1 + size)));
-	APAC_ASSERT(curr_cpu.apn_addmul_one_ptr != NULL);
+	APAC_ASSERT(curr_cpu.apn_addmul_one_ptr != NULL,
+		"apacGetCPUSpec() or apacInit() not called");
+
+	// Prevent partial overlap between result and op1.
+	// Allowed only if result is entirely before or after op1's range.
+	APAC_ASSERT((result <= op1 - 1) || (result >= op1 + size),
+		"(result) overlaps the middle of (op1). Allowed only if (result) <= (op1 - 1) "
+		"or (result) >= (op1 + size)");
 
 	apn_seg_t out_val = curr_cpu.apn_addmul_one_ptr(result, op1, size, val);
 	return out_val;
