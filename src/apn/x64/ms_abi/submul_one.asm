aa76c5bf9752bdcefeba8f7edd84e68bffc83a80
The Blessed Machine <devanshmehrotra117@gmail.com>
2025-12-23 20:04:52 +0530

    more bugs in submul_one_zen4 fixed, this variant now uses adox/adcx/mulx.

--- src/apn/x64/ms_abi/submul_one.asm
+++ src/apn/x64/ms_abi/submul_one.asm
@@ -36,32 +36,36 @@ start_of_func:
     xchg    r11, rdx    ; r11 <- op1_ptr
     mov     rcx, r8     ; rcx <- size
     mov     rdx, r9     ; rdx <- val
-    and     r8,  7      ; size %= 8
-    shr     rcx, 3      ; size /= 8
-    xor     r9,  r9     ; temp_reg = 0
-    test    rcx, rcx    
+    and     r8,  3      ; size %= 4
+    shr     rcx, 2      ; size /= 4
+    xor     r9,  r9     ; temp_reg1 = 0
+    test    rcx, rcx
+    stc                 ; set carry flag
     jz      before_remainder
 
 ALIGN 64
 unrolled_loop:
 
 i = 0
-WHILE i LT 8
+WHILE i LT 4
 
     mulx    rdi, rsi, QWORD PTR [r11 + i * 8]
-    add     r9,  rsi
-    adc     rdi, 0
-    sub     QWORD PTR [r10 + i * 8], r9
+    adox    rsi, r9
+    not     rsi
+    adcx    rsi, QWORD PTR [r10 + i * 8]
+    mov     QWORD PTR [r10 + i * 8], rsi
     mov     r9,  rdi
 
 i = i + 1
 ENDM
      
-     lea    r11, [r11 + 64]
-     lea    r10, [r10 + 64]
-     dec    rcx
-     jnz    unrolled_loop
+    lea     r11, [r11 + 32]
+    lea     r10, [r10 + 32]
+    lea     rcx, [rcx - 1]
+    jrcxz   before_remainder
+    jmp     unrolled_loop
 
+ALIGN 16
 before_remainder:
 
     mov     rcx, r8
@@ -70,25 +74,28 @@ before_remainder:
 remainder_loop:
 
     mulx    rdi, rsi, QWORD PTR [r11]
-    add     r9,  rsi
-    adc     rdi, 0
-    sub     QWORD PTR [r10], r9
+    adox    rsi, r9
+    not     rsi
+    adcx    rsi, QWORD PTR [r10]
+    mov     QWORD PTR [r10], rsi
     mov     r9,  rdi
 
     lea     r11, [r11 + 8]
     lea     r10, [r10 + 8]
-    dec     rcx
-    jnz     remainder_loop
+    loop    remainder_loop
 
 end_of_loop:
 
-    sbb     QWORD PTR [r10], r9
+    adox    r9,  rcx
+    not     r9
+    adcx    r9,  QWORD PTR [r10]
+    mov     QWORD PTR [r10], r9
 
 end_of_func:
 
-    setc    al
+    setnc   al
     movzx   rax, al
-    
+
     pop     rsi
     pop     rdi
     ret
