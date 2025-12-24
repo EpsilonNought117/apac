2368a742ebe512db195062409cc1f51f4143c5a0
The Blessed Machine <devanshmehrotra117@gmail.com>
2025-12-24 10:39:14 +0530

    added 64-bit definitions for APN_SEG

--- include/apac.h
+++ include/apac.h
@@ -97,17 +97,18 @@
 
 #endif
 
-typedef size_t  apn_size_t;
-
+typedef size_t              apn_size_t;
 #define PRI_APN_PTR         "p"
 #define PRI_APN_SIZE        "zu"
 #define APN_SIZE_MAX        SIZE_MAX
 
 #if (SIZE_OF_VOID_PTR == 8U)
 
-    typedef uint64_t        apn_seg_t;
-    #define APN_SEG_MAX     UINT64_MAX
-    #define PRI_APN_SEG     PRIu64
+    typedef uint64_t            apn_seg_t;
+    #define APN_SEG_MAX         UINT64_MAX
+    #define PRI_APN_SEG         PRIu64
+    #define APN_SEG_BITS        64U
+    #define APN_SEG_HIGH_BIT    (1ULL << 63)
 
 #endif
 
