65f006f57c0dc421880a4af8034369b7d2f80ab9
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2025-12-29 17:53:06 +0530

    bug in CPUID macro fixed for GCC/Clang invocations

--- src/misc/apacGetCPUSpec.c
+++ src/misc/apacGetCPUSpec.c
@@ -18,14 +18,22 @@ extern void generic_x64_set_params(void);
 
 #elif (defined(__GNUC__) || defined(__clang__))
 
-#define CPUID(cpuInfo, Leaf)	\
-		__cpuid((Leaf), &cpuInfo[0], &cpuInfo[1], &cpuInfo[2], &cpuInfo[3])
+    #define CPUID(cpuInfo, leaf)        \
+        __cpuid((leaf),                 \
+                (cpuInfo)[0],           \
+                (cpuInfo)[1],           \
+                (cpuInfo)[2],           \
+                (cpuInfo)[3])
 
-#define CPUIDEX(cpuInfo, Leaf, SubLeaf)		\
-		__cpuid_count((Leaf), (SubLeaf), &cpuInfo[0], &cpuInfo[1], &cpuInfo[2], &cpuInfo[3])
+    #define CPUIDEX(cpuInfo, leaf, subleaf)          \
+        __cpuid_count((leaf), (subleaf),             \
+                    (cpuInfo)[0],                    \
+                    (cpuInfo)[1],                    \
+                    (cpuInfo)[2],                    \
+                    (cpuInfo)[3])
 
 #else
-#error "Unsupported Compiler!"
+    #error "Unsupported Compiler!"
 #endif
 
 void apacGetCPUSpec(void)
