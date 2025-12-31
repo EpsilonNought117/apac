6d29e549322b7c78cb3aa1eeddeb0f5f4afa30ae
The Blessed Machine <devanshmehrotra117@gmail.com>
2025-12-31 10:17:05 +0530

    more descriptive labels

--- src/apn/x64/sysv_abi/add_n.s
+++ src/apn/x64/sysv_abi/add_n.s
@@ -1,3 +1,4 @@
+
 #   O---------------------------------------------------------------------------O
 #   |                                                                           |
 #   |                   BALANCED ADDITION FUNCS (N LIMBS)                       |
@@ -21,31 +22,32 @@ add_n_zen4:
 .cfi_startproc
 
     xor     rax, rax
-    mov     r11, rcx        # r11 = size
-    shr     rcx, 2          # rcx = size / 4
-    and     r11, 3          # r11 = size % 4
-    jz      2f
+    mov     r11, rcx
+    shr     rcx, 2
+    and     r11, 3
+    jz      .Lzen4_before_main_loop
 
-1:
+.Lzen4_rmdr_loop:
+
     mov     rax, [rsi]
     adc     rax, [rdx]
     mov     [rdi], rax
-
     lea     rsi, [rsi + 8]
     lea     rdx, [rdx + 8]
     lea     rdi, [rdi + 8]
-
     dec     r11
-    jnz     1b
+    jnz     .Lzen4_rmdr_loop
 
-2:
+.Lzen4_before_main_loop:
+
     setc    al
     test    rcx, rcx
     bt      ax, 0
-    jz      4f
+    jz      .Lzen4_end_of_func
 
-.p2align 4
-3:
+.p2align 6
+.Lzen4_unroll4_loop:
+
 .set i, 0
 .rept 4
 
@@ -59,12 +61,12 @@ add_n_zen4:
     lea     rsi, [rsi + 32]
     lea     rdx, [rdx + 32]
     lea     rdi, [rdi + 32]
-
     dec     rcx
-    jnz     3b
+    jnz     .Lzen4_unroll4_loop
 
 .p2align 4
-4:
+.Lzen4_end_of_func:
+
     setc    al
     movzx   rax, al
     ret
@@ -77,27 +79,28 @@ add_n_x64:
 
     xor     rax, rax
     test    rcx, rcx
-    jz      2f
+    jz      .Lx64_end_of_func
 
 .p2align 4
-1:
+.Lx64_main_loop:
+
     mov     rax, QWORD PTR [rsi]
     adc     rax, QWORD PTR [rdx]
     mov     QWORD PTR [rdi], rax
-    
+
     lea     rsi, [rsi + 8]
     lea     rdx, [rdx + 8]
     lea     rdi, [rdi + 8]
-    
+
     dec     rcx
-    jnz     1b
+    jnz     .Lx64_main_loop
 
 .p2align 4
-2:
+.Lx64_end_of_func:
+
     setc    al
     movzx   rax, al
     ret
 
 .cfi_endproc
 .size add_n_x64, .-add_n_x64
-
