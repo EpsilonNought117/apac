196f1d5191d2e1f3c8a7571c5bdfd1d78b05c64f
EpsilonNought117 <devanshmehrotra117@gmail.com>
2026-06-11 11:55:51 +0530

    added apz_free, updated cmakelists.txt

---
+++ src/apz/apz_free.c
@@ -0,0 +1,16 @@
+#include "../header/apac_internal.h"
+
+apac_err
+apz_free(
+    apz_t* op1
+)
+{
+    APAC_ASSERT(op1 != NULL);
+
+    apac_free(op1->num);
+    op1->curr = 0;
+    op1->max = 0;
+    op1->sign = APZ_POS;
+
+    return APAC_OK;
+}
